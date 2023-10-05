0624b1fae78d4a84774f8c355b77358dac86a114
kaulaskar <saileshkaulaskar@gmail.com>
2023-10-05 16:00:12 +0530

    modified the init.tf file

--- init.tf
+++ init.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "devopsremotestate1"
-    key    = "dev/module.tfstate"
+    bucket = "devopsremotestate1" #give bucket name in your s3
+    key    = "dev/module.tfstate"  #give path in your s3
     region = "us-east-1"
   }
 }
