7751fdc936d654538e09efa88533110f28c75ea0
hemanth-chittela <114971743+hemanth-chittela@users.noreply.github.com>
2023-10-27 09:52:26 +0530

    new change

--- lambda_function.py
+++ lambda_function.py
@@ -1,18 +1,22 @@
-#
 import boto3
 import time
 from datetime import datetime, timedelta
 AWS_REGION = 'ap-south-1'  # Replace with your desired region
-EC2_INSTANCE_ID = 'give your instance id'  # Replace with your EC2 instance ID
-CPU_THRESHOLD = 70.0  # Set the CPU utilization threshold
+EC2_INSTANCE_ID = 'i-00449734175769b0d'  # Replace with your EC2 instance ID
+CPU_THRESHOLD = 2.0  # Set the CPU utilization threshold
 WAIT_TIME_SECONDS = 300
+
+
+
 
 def lambda_handler(event, context):
     # Initialize the EC2 client
     ec2 = boto3.client('ec2', region_name=AWS_REGION)
-
+    
+    instance_status = ec2.describe_instance_status(InstanceIds=[EC2_INSTANCE_ID])
+    if not instance_status['InstanceStatuses']:
     # Start the specified EC2 instance
-    ec2.start_instances(InstanceIds=[EC2_INSTANCE_ID])
+        ec2.start_instances(InstanceIds=[EC2_INSTANCE_ID])
     
     print(f'Starting the instance: {EC2_INSTANCE_ID}')
 
@@ -36,7 +40,7 @@ def lambda_handler(event, context):
     topic_arn = response['TopicArn']
     
     subscriptions = sns.list_subscriptions_by_topic(TopicArn=topic_arn)
-    email_address = 'give your email address'  # Replace with your email address
+    email_address = 'sandwitch132@gmail.com'  # Replace with your email address
     
     if not any(subscription['Protocol'] == 'email' and subscription['Endpoint'] == email_address for subscription in subscriptions['Subscriptions']):
         # Email address is not subscribed, so subscribe it
@@ -58,7 +62,7 @@ def lambda_handler(event, context):
         Threshold=CPU_THRESHOLD,  # Set the threshold for CPU utilization
         ActionsEnabled=True,  # Enable actions
         AlarmActions=[  # Specify the actions to take when the alarm state is triggered
-            'give your ARN here',  # Replace with your SNS topic ARN
+            'arn:aws:sns:ap-south-1:619436695148:tester.fifo',  # Replace with your SNS topic ARN
         ],
         Dimensions=[
             {
@@ -94,14 +98,18 @@ def lambda_handler(event, context):
         StartTime=start_time,
         EndTime=end_time
     )
-    values = response['MetricDataResults'][0]['Values']
-    if all(value < CPU_THRESHOLD for value in values):
+    values = response['MetricDataResults'][0].get('Values', [])
+    if values:
+        print(f'CPU utilization values at 5 minutes: {values}')
+        print(f'CPU utilization at 5 minutes: {values[-1]}%')
+        
+    if any(value >= CPU_THRESHOLD for value in values):
+        print(f'CPU utilization is above the {CPU_THRESHOLD}. Instance is not stopped.')
+    else:
         sns.publish(
             TopicArn=topic_arn,  # Use the ARN of the created topic
             Message=f"EC2 instance {EC2_INSTANCE_ID} is being stopped due to inactivity.",
             Subject="EC2 Instance Inactivity Alert"
         )
-        ec2.stop_instances(InstanceIds=[EC2_INSTANCE_ID])
-        print(f'CPU utilization is below the threshold. Instance is stopped.')
-    else:
-        print(f'CPU utilization exceeded the threshold. Instance is not stopped.')
+    ec2.stop_instances(InstanceIds=[EC2_INSTANCE_ID])
+    print(f'CPU utilization below the threshold. Hence Instance is stopped')
