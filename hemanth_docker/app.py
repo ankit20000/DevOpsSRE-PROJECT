9ec94284d0024ac49480d9cf123c5c403607e316
Ubuntu <ubuntu@ip-172-31-33-232.ap-south-1.compute.internal>
2023-11-03 13:04:18 +0000

    new files

---
+++ hemanth_docker/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def hey():
+    return redirect(url_for('hello'))
+
+@app.route('/health')
+def hello():
+    return "hello world"
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=80)
