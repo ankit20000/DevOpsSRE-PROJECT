aabe9882dd60364b587acae06716ee0d04f5d65f
anveshreddy52 <anveshreddy5256@gmail.com>
2024-05-18 18:59:42 +0530

    just now

--- anusha/terraform/subnet.tf
+++
@@ -1,9 +0,0 @@
-#create public subnet
-resource "aws_subnet" "public-subnet" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = var.subnet_cidr
-
-  tags = {
-    Name = var.public_subnet_name
-  }
-}
